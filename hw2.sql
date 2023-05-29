30bcab09bf0c1f9230207df883f4fc42901a816a
Krylov Egor <KrylovWork@yandex.ru>
2023-05-29 23:45:44 +0500

    hw2 done

---
+++ hw2.sql
@@ -0,0 +1,49 @@
+CREATE DATABASE hw_2;
+
+USE hw_2;
+
+CREATE TABLE sales
+(
+	id SERIAL PRIMARY KEY,
+    order_date DATE NOT NULL,
+    count_product INT NOT NULL
+);
+
+INSERT INTO sales (order_date, count_product)
+
+VALUES ('2022-01-01', 156),
+('2022-01-02', 180),
+('2022-01-03', 21), 
+('2022-01-04', 124), 
+('2022-01-05', 341);
+
+SELECT id AS 'id заказа', 
+IF (count_product < 100, 'Маленький заказ', 
+	IF (count_product < 300, 'Средний заказ', 'Большой заказ')) AS 'Тип заказа'
+FROM sales;
+
+CREATE TABLE orders
+(
+	id SERIAL PRIMARY KEY,
+    employee_id VARCHAR(5) NOT NULL,
+    amount NUMERIC(7, 2) NOT NULL, 
+    order_status VARCHAR(15) NOT NULL
+);
+
+INSERT INTO orders (employee_id, amount, order_status)
+
+VALUES ('e03', 15.00, 'OPEN'),
+('e01', 25.50, 'OPEN'),
+('e05', 100.70, 'CLOSED'), 
+('e02', 22.18, 'OPEN'), 
+('e04', 9.50, 'CANCELLED');
+
+SELECT *, 
+CASE order_status
+	WHEN 'OPEN' THEN 'Order is in open state'
+	WHEN 'CLOSED' THEN 'Order is closed'
+	WHEN 'CANCELLED' THEN 'Order is cancelled'
+ELSE ''
+END AS full_order_status
+FROM orders;
+    
